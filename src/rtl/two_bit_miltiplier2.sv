28256366f98a028cf906f40fd93ce93123609955
Eric Dong <ericdong@cad.eecs.harvard.edu>
2022-11-08 04:28:23 -0500

    2 cycle

--- src/rtl/two_bit_miltiplier2.sv
+++ src/rtl/two_bit_miltiplier2.sv
@@ -11,24 +11,30 @@ module two_bit_multiplier2 #(
 );
 
 	/**************************** ENUM DECLARATION ******************************/
+	enum logic [1:0] {
+		IDLE, MUL, RESULT
+	} state, next_state;
 
 	/**************************** LOGIC DECLARATION *****************************/
-	logic [$clog2(N):0] 	first_one, second_one;
+	// logic [$clog2(N):0] 	first_one, second_one;
 	logic [N-1:0]       	b_reverse;
 	logic 					num_one_bits;
 
-	logic [31:0]			first_shifted, second_shifted;
+	// logic [31:0]			first_shifted, second_shifted;
 
-	assign first_one		= find_first_one_bit_pos(b);
-	assign second_one   	= N - 1 - find_first_one_bit_pos(b_reverse);
+	// assign first_one		= find_first_one_bit_pos(b);
+	// assign second_one   	= N - 1 - find_first_one_bit_pos(b_reverse);
 	assign num_one_bits 	= ^b;
 
-	assign first_shifted	= a << first_one;
-	assign second_shifted	= a << second_one;
+	// assign first_shifted	= a << first_one;
+	// assign second_shifted	= a << second_one;
 
 	/*************************** GENERATE DECLARATION ***************************/
 
 	/**************************** TASK DECLARATION ******************************/
+	task reset();
+		state 		<= IDLE;
+	endtask : reset
 
 	/**************************** FUNC DECLARATION ******************************/
 	function logic [$clog2(N):0] find_first_one_bit_pos(logic [N-1:0] local_b);
@@ -37,25 +43,6 @@ module two_bit_multiplier2 #(
 			temp = local_b[i] ? i : temp; 
 		end 
 		return temp;
-		// priority case(1'b1)
-		// 	local_b[3]: begin
-		// 		return 3; 
-		// 	end 
-
-		// 	local_b[2]: begin
-		// 		return 2; 
-		// 	end 
-
-		// 	local_b[1]: begin
-		// 		return 1; 
-		// 	end 
-
-		// 	local_b[0]: begin
-		// 		return 0; 
-		// 	end 
-
-		// 	default:;
-		// endcase
 	endfunction
 
 	/******************************* COMB BLOCKS ********************************/
@@ -66,17 +53,48 @@ module two_bit_multiplier2 #(
 	end
 	
 	always_comb begin : MULTIPLY
-		c 						= '0;
 		result_vld 				= '0;
-		if (vld) begin 
-			if (b != '0) begin
-				c 				= first_shifted + (num_one_bits ? 0 : second_shifted);
-				// $display("%d, %d, %d", a, b, c);
-			end
-			result_vld = 1'b1;
-		end
+		next_state = state;
+		unique case(state)
+			IDLE: begin 
+				if (vld) begin
+					next_state 	= MUL;
+				end
+			end 
+
+			MUL: begin 
+				next_state 		= RESULT;
+			end 
+
+			RESULT: begin 
+				next_state		= IDLE;
+				result_vld		= 1'b1;
+			end 
+		endcase
 	end
 
 	/******************************* PROC BLOCKS ********************************/
+	always_ff @(posedge clk) begin
+		if(~rst_n) begin
+			reset();
+		end else begin
+			state <= next_state;
+			unique case(state)
+				IDLE: begin 
+					if (vld) begin
+						c 			<= a << find_first_one_bit_pos(b);
+					end
+				end 
+
+				MUL: begin 
+					c 				<= b == 0 ? '0 : (c + (num_one_bits ? 0 : a << (N - 1 - find_first_one_bit_pos(b_reverse))));
+				end 
+
+				RESULT: begin
+					c  				<= '0;
+				end 
+			endcase
+		end
+	end
 
 endmodule
